d8e0155cf0b968657d29dedf7630738690b38620
Pratik Kharadhara <pratikkharadhara@gmail.com>
2026-04-01 02:28:18 +0530

    added routes and navbar for favourites and home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import MovieCard from "./Elements/MovieCard"
+import Favourties from "./Pages/Favourites"
 import Home from "./Pages/Home"
+import { Routes ,Route } from "react-router-dom"
+import NavBar from "./Elements/NavBar"
 function App() {
   
 
   return (
     <>
-      <Home/>
+    {/* this is our nav where all the links will be there to go between various pages */}
+      <NavBar />
+    {/* so this is use for making the route between various routes/pages */}
+     <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favourites" element={<Favourties />}/>
+     </Routes>
      </>
   )
 }
---
+++ frontend/src/Pages/Favourites.jsx
@@ -0,0 +1,9 @@
+export default function Favourties(){
+    return (
+    
+        <div className="fav-page">
+            <h2>there is no favourites right now </h2>
+            <p>add favourites to this list</p>
+        </div>
+    )
+}
